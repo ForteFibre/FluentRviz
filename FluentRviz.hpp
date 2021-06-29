bd6edfa13c20bca3870f2c66536c302fac54a955
Minami Shota <shouth373@gmail.com>
2021-06-30 03:40:18 +0900

    マーカーの種別に応じた実装の追加

--- FluentRviz.hpp
+++ FluentRviz.hpp
@@ -15,7 +15,7 @@ template<int32_t TYPE>
 struct ActionType {
     template<typename Derived, typename Base>
     struct Decorator : Base {
-        Decorator()
+        Decorator() noexcept
         {
             this->message.action = TYPE;
         }
@@ -26,7 +26,7 @@ template<int32_t TYPE>
 struct MarkerType {
     template<typename Derived, typename Base>
     struct Decorator : Base {
-        Decorator()
+        Decorator() noexcept
         {
             this->message.type = TYPE;
         }
@@ -46,7 +46,7 @@ struct Position : Base {
 
 template<typename Derived, typename Base>
 struct Orientation : Base {
-    Orientation()
+    Orientation() noexcept
     {
         orientation(1, 0, 0, 0);
     }
@@ -63,7 +63,7 @@ struct Orientation : Base {
 
 template<typename Derived, typename Base>
 struct Scale : Base {
-    Scale()
+    Scale() noexcept
     {
         scale(1, 1, 1);
     }
@@ -78,8 +78,83 @@ struct Scale : Base {
 };
 
 template<typename Derived, typename Base>
+struct PoseArrowScale : Base {
+    PoseArrowScale() noexcept
+    {
+        scale(0.2, 0.2, 1);
+    }
+
+    Derived &scale(const double length, const double width, const double height) noexcept
+    {
+        this->marker.scale.x = length;
+        this->marker.scale.y = width;
+        this->marker.scale.z = height;
+        return this->derived();
+    }
+};
+
+template<typename Derived, typename Base>
+struct VectorArrowScale : Base {
+    VectorArrowScale() noexcept
+    {
+        scale(0.2, 0.4, 0.4);
+    }
+
+    Derived &scale(const double shaft_diameter, const double head_diameter, const double head_length) noexcept
+    {
+        this->message.scale.x = shaft_diameter;
+        this->message.scale.y = head_diameter;
+        this->message.scale.z = head_length;
+        return this->derived();
+    }
+};
+
+template<typename Derived, typename Base>
+struct PointScale : Base {
+    PointScale() noexcept
+    {
+        scale(0.05, 0.05);
+    }
+
+    Derived &scale(const double width, const double height) noexcept
+    {
+        this->marker.scale.x = width;
+        this->marker.scale.y = height;
+        return this->derived();
+    }
+};
+
+template<typename Derived, typename Base>
+struct LineScale : Base {
+    LineScale() noexcept
+    {
+        scale(0.05);
+    }
+
+    Derived &scale(const double width) noexcept
+    {
+        this->marker.scale.x = width;
+        return this->derived();
+    }
+};
+
+template<typename Derived, typename Base>
+struct TextScale : Base {
+    TextScale() noexcept
+    {
+        scale(0.05);
+    }
+
+    Derived &scale(const double height) noexcept
+    {
+        this->marker.scale.z = height;
+        return this->derived();
+    }
+};
+
+template<typename Derived, typename Base>
 struct Color : Base {
-    Color()
+    Color() noexcept
     {
         color(1, 1, 1);
     }
@@ -95,7 +170,7 @@ struct Color : Base {
 
 template<typename Derived, typename Base>
 struct Colors : Base {
-    Colors()
+    Colors() noexcept
     {
         color(1, 1, 1);
     }
@@ -126,8 +201,37 @@ struct Points : Base {
 };
 
 template<typename Derived, typename Base>
+struct ArrowPoints : Base {
+    ArrowPoints() noexcept
+    {
+        this->message.points.resize(2);
+    }
+
+    Derived &start(const double x, const double y, const double z) noexcept
+    {
+        return set(0, x, y, z);
+    }
+
+    Derived &end(const double x, const double y, const double z) noexcept
+    {
+        return set(1, x, y, z);
+    }
+
+private:
+    Derived &set(size_t index, const double x, const double y, const double z) noexcept
+    {
+        geometry_msgs::Point point;
+        point.x = x;
+        point.y = y;
+        point.z = z;
+        this->message.points[index] = point;
+        return this->derived();
+    }
+};
+
+template<typename Derived, typename Base>
 struct Text : Base {
-    Text()
+    Text() noexcept
     {
         text("visualization_msgs::Marker");
     }
@@ -156,7 +260,7 @@ struct MeshResource : Base {
 
 template<typename Derived, typename Base>
 struct CRTP : Base {
-    Derived &derived()
+    Derived &derived() noexcept
     {
         return static_cast<Derived &>(*this);
     }
@@ -252,11 +356,11 @@ struct Add
 
 using PoseArrowMarker = Add<
     visualization_msgs::Marker::ARROW,
-    Position, Orientation, Scale, Color>;
+    Position, Orientation, PoseArrowScale, Color>;
 
 using VectorArrowMarker = Add<
     visualization_msgs::Marker::ARROW,
-    Scale, Color, Points>;
+    Scale, Color, VectorArrowScale, ArrowPoints>;
 
 using CubeMarker = Add<
     visualization_msgs::Marker::CUBE,
@@ -272,27 +376,27 @@ using CylinderMarker = Add<
 
 using LineStripMarker = Add<
     visualization_msgs::Marker::LINE_STRIP,
-    Position, Orientation, Scale, Color, Points>;
+    Position, Orientation, LineScale, Color, Points>;
 
 using LineListMarker = Add<
     visualization_msgs::Marker::LINE_LIST,
-    Position, Orientation, Scale, Color, Points>;
+    Position, Orientation, LineScale, Colors, Points>;
 
 using CubeListMarker = Add<
     visualization_msgs::Marker::CUBE_LIST,
-    Position, Orientation, Scale, Color, Points>;
+    Position, Orientation, Scale, Colors, Points>;
 
 using SphereListMarker = Add<
     visualization_msgs::Marker::SPHERE_LIST,
-    Position, Orientation, Scale, Color, Points>;
+    Position, Orientation, Scale, Colors, Points>;
 
 using PointsMarker = Add<
     visualization_msgs::Marker::POINTS,
-    Position, Orientation, Scale, Color, Points>;
+    Position, Orientation, PointScale, Colors, Points>;
 
 using TextViewFacingMarker = Add<
     visualization_msgs::Marker::TEXT_VIEW_FACING,
-    Position, Scale, Color, Text>;
+    Position, TextScale, Color, Text>;
 
 using MeshResourceMarker = Add<
     visualization_msgs::Marker::MESH_RESOURCE,
@@ -300,5 +404,5 @@ using MeshResourceMarker = Add<
 
 using TriangleListMarker = Add<
     visualization_msgs::Marker::TRIANGLE_LIST,
-    Position, Orientation, Scale, Color, Points>;
+    Position, Orientation, Scale, Colors, Points>;
 }
