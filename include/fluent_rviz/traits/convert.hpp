10428c44c7b20cb5487647ca6130a02c9303f280
Shota Minami <shouth373@gmail.com>
2023-07-22 02:51:11 +0000

    Add identity conversion

--- include/fluent_rviz/traits/convert.hpp
+++ include/fluent_rviz/traits/convert.hpp
@@ -5,6 +5,13 @@ namespace flrv::traits
 template <typename Into, typename From, typename Enabler = void>
 struct Convert;
 
+template <typename T>
+struct Convert<T, T>
+{
+  static auto do_convert(const T &value) -> T
+  { return value; }
+};
+
 template <typename Into, typename From>
 [[nodiscard]]
 auto convert(const From &from) -> decltype(auto)
