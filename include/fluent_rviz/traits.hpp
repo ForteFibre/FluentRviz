2e628a3506f858065d9d582ea24978a45d882863
Shota Minami <shouth373@gmail.com>
2023-07-29 06:24:37 +0000

    Update traits

--- include/fluent_rviz/traits.hpp
+++ include/fluent_rviz/traits.hpp
@@ -1,7 +1,12 @@
 #pragma once
 
+#include <algorithm>
 #include <cstddef>
+#include <initializer_list>
+#include <iterator>
+#include <type_traits>
 #include <utility>
+#include <vector>
 
 namespace flrv::traits
 {
@@ -11,18 +16,61 @@ using Require = std::nullptr_t;
 template <typename Into, typename From, typename Enabler = void>
 struct Convert;
 
+template <typename Into, typename From>
+using ConversionDefined =
+  decltype(Convert<Into, std::remove_cv_t<std::remove_reference_t<From>>>::do_convert(std::declval<From>()));
+
 template <typename T>
 struct Convert<T, T>
 {
   static auto do_convert(const T &value) -> T
   { return value; }
+
+  static auto do_convert(T &&value) -> T
+  { return std::move(value); }
 };
 
 template <typename Into, typename From>
-using ConversionDefined = decltype(Convert<Into, From>::do_convert(std::declval<From>()));
+struct Convert<std::vector<Into>, std::vector<From>, std::void_t<ConversionDefined<Into, From>>>
+{
+  static auto do_convert(const std::vector<From> &from) -> std::vector<Into>
+  {
+    auto into = std::vector<Into>{ };
+    into.reserve(from.size());
+    for (const auto &e : from) {
+      into.emplace_back(Convert<Into, From>::do_convert(e));
+    }
+    return into;
+  }
+
+  template<
+    traits::Require<
+      std::enable_if_t<std::is_same_v<From, Into>>> = nullptr>
+  static auto do_convert(std::vector<From> &&from) -> std::vector<Into>
+  { return std::move(from); }
+};
+
+template <typename Into, typename From>
+struct Convert<std::vector<Into>, std::initializer_list<From>, std::void_t<ConversionDefined<Into, From>>>
+{
+  static auto do_convert(std::initializer_list<From> from) -> std::vector<Into>
+  {
+    auto into = std::vector<Into>{ };
+    into.reserve(from.size());
+    for (const auto &e : from) {
+      into.emplace_back(Convert<Into, From>::do_convert(e));
+    }
+    return into;
+  }
+};
+
+template <typename Into, typename From>
+[[nodiscard]]
+auto convert(const From &from) -> decltype(auto)
+{ return Convert<Into, From>::do_convert(from); }
 
 template <typename Into, typename From>
 [[nodiscard]]
 auto convert(From &&from) -> decltype(auto)
-{ return Convert<Into, From>::do_convert(std::forward<From>(from)); }
+{ return Convert<Into, From>::do_convert(std::move(from)); }
 }  // namespace flrv::traits
