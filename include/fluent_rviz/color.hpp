d3851a8eba352470f9daffb88d9a69556fb8e8f6
Shota Minami <shouth373@gmail.com>
2023-08-18 12:50:00 +0000

    Update creation functions

--- include/fluent_rviz/color.hpp
+++ include/fluent_rviz/color.hpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <cmath>
 #include <cstddef>
+#include <cstdint>
 #include <type_traits>
 
 #include <std_msgs/msg/color_rgba.hpp>
@@ -28,39 +29,48 @@ struct Color : public std_msgs::msg::ColorRGBA
 };
 
 [[nodiscard]]
-inline auto RGBA(const float r, const float g, const float b, const float a = 1.0) noexcept -> Color
-{ return { r, g, b, a }; }
+inline auto RGBA(float r, float g, float b, float a = 1.f) noexcept -> Color
+{
+  r = std::clamp(r, 0.f, 1.f);
+  g = std::clamp(g, 0.f, 1.f);
+  b = std::clamp(b, 0.f, 1.f);
+  a = std::clamp(a, 0.f, 1.f);
+  return { r, g, b, a };
+}
 
 [[nodiscard]]
-inline auto HSLA(float h, float s, float l, float a = 1.0) noexcept -> Color
+inline auto HSLA(float h, float s, float l, float a = 1.f) noexcept -> Color
 {
   h = std::fmod(h, 360.f);
   if (h < 0) {
-    h += 360;
+    h += 360.f;
   }
   s = std::clamp(s, 0.f, 1.f);
   l = std::clamp(l, 0.f, 1.f);
   a = std::clamp(a, 0.f, 1.f);
 
-  auto c = s * (1.f - std::abs(2.f * l - 1.f));
-  auto x = c * (1.f - std::abs(std::fmod(h / 60.f, 2.f) - 1.f));
-  auto m = l - c / 2.f;
-  c += m;
-  x += m;
+  auto f = [&](float n) {
+    auto k = std::fmod(n + h / 30.f, 12.f);
+    return l - s * std::min(l, 1.f - l) * std::max(-1.f, std::min({ k - 3.f, 9.f - k, 1.f }));
+  };
+  return { f(0.f), f(8.f), f(4.f), a };
+}
 
-  if (0 <= h and h < 60) {
-    return { c, x, 0, a };
-  } else if (60 <= h and h < 120) {
-    return { x, c, 0, a };
-  } else if (120 <= h and h < 180) {
-    return { 0, c, x, a };
-  } else if (180 <= h and h < 240) {
-    return { 0, x, c, a };
-  } else if (240 <= h and h < 300) {
-    return { x, 0, c, a };
-  } else {
-    return { c, 0, x, a };
+[[nodiscard]]
+inline auto HWBA(float h, float w, float b, float a = 1.f) noexcept -> Color
+{
+  w = std::clamp(w, 0.f, 1.f);
+  b = std::clamp(b, 0.f, 1.f);
+  if (w + b > 1.f) {
+    auto g = w / (w + b);
+    return { g, g, g, a };
   }
+
+  auto [ rr, gg, bb, aa ] = HSLA(h, 1.f, .5f, a);
+  auto f = [&](float c) {
+    return c * (1.f - w - b) + w;
+  };
+  return { f(rr), f(gg), f(bb), aa };
 }
 }  // namespace flrv::color
 
